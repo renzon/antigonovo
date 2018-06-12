e66f685989e8d936a93c10ba240977701ed30f1c
renzon <renzon@gmail.com>
2018-06-11 23:15:28 -0300

    Criado home com lib Twitter Bootstrap
    close #22

--- antigonovo/settings.py
+++ antigonovo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'antigonovo.core'
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
