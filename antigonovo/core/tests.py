d5e263de95cd0dcb3599b870287241e95aff0a5c
renzon <renzon@gmail.com>
2018-06-11 22:12:11 -0300

    Criada e testada mensagem Olá Mundo
    close #18

--- antigonovo/core/tests.py
+++ antigonovo/core/tests.py
@@ -1,2 +1,11 @@
-def test_home():
-    assert 1 == 1
+from antigonovo.django_assertions import dj_assert_contains
+
+
+def test_status_code(client):
+    response = client.get('/')
+    assert 200 == response.status_code
+
+
+def test_home(client):
+    response = client.get('/')
+    dj_assert_contains(response, 'Olá Mundo')
