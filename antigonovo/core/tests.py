7fc2e99fbc539b48d2fbfdabf71ffb310139e05f
renzon <renzon@gmail.com>
2018-06-20 22:03:08 -0300

    Feito design da página de contatos, extraido base.html
    close #35

--- antigonovo/core/tests.py
+++ antigonovo/core/tests.py
@@ -3,7 +3,7 @@ import pytest
 from antigonovo.django_assertions import dj_assert_contains
 
 
-def test_status_code(client):
+def test_home_status_code(client):
     response = client.get('/')
     assert 200 == response.status_code
 
@@ -13,10 +13,30 @@ def test_status_code(client):
         'Antigo Novo Móveis',
         'contato@antigonovo.com.br',
         '+55 12 987654-3210',
+        'href="/contato"'
 
     ]
 )
 def test_home(client, content):
     response = client.get('/')
+    dj_assert_contains(response, content)
 
+
+def test_contact_status_code(client):
+    response = client.get('/contato/')
+    assert 200 == response.status_code
+
+
+@pytest.mark.parametrize(
+    'content', [
+        '(12) 98873-9669',
+        '(12) 98802-1263',
+        '(12) 99799-1571',
+        'SP 50 (estrada SJC / Monteiro Lobato) Nº 20.200, KM 20',
+        'São José dos Campos - SP',
+        '12220-340',
+    ]
+)
+def test_contact_content(client, content):
+    response = client.get('/contato/')
     dj_assert_contains(response, content)
--- antigonovo/core/views.py
+++ antigonovo/core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'core/home.html')
+
+
+def contact(request):
+    return render(request, 'core/contact_detail.html')
