a0a12d8ed6521984c1d8932c2e27fb51e7b8706a
renzon <renzon@gmail.com>
2018-06-27 21:49:15 -0300

    Passado valor de modelo (objeto) Python Puro para template
    close #53

--- antigonovo/moveis/views.py
+++ antigonovo/moveis/views.py
@@ -1,7 +1,28 @@
+from decimal import Decimal
+
 from django.shortcuts import render
 
-
 # Create your views here.
+from antigonovo.moveis.models import Movel
+
 
 def index(request):
-    return render(request, 'moveis/index.html')
+    description = 'Prensa de farinha de mandioca, madeira maciça. Construída artesanalmente por ' \
+                  'escravos.'
+    ctx = {
+        'moveis':
+            [
+                Movel(
+                    'Prensa de Madeira da Época Colonial',
+                    Decimal('30000.00'),
+                    description
+                ),
+                Movel(
+                    'Mesa',
+                    Decimal('4000.00'),
+                    'Mesa em madeira de lei'
+                ),
+            ]
+    }
+
+    return render(request, 'moveis/index.html', context=ctx)
