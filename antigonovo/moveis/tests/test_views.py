fccd450ddee7938848865c72e10faa54d43a2198
renzon <renzon@gmail.com>
2018-07-02 22:23:25 -0300

    Feito upload de Imagens
    close #62

--- antigonovo/moveis/tests/test_views.py
+++ antigonovo/moveis/tests/test_views.py
@@ -1,4 +1,7 @@
+from os import path
+
 import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
 from antigonovo.django_assertions import dj_assert_contains
@@ -10,13 +13,21 @@ def test_app_link_in_home(client):
     dj_assert_contains(response, reverse('moveis:index'))
 
 
+IMAGE_PATH = path.dirname(__file__)
+IMAGE_PATH = path.join(IMAGE_PATH, 'prensa.jpg')
+
+
 @pytest.fixture
 def moveis(db):
+    image = SimpleUploadedFile(
+        name='prensa.jpg', content=open(IMAGE_PATH, 'rb').read(), content_type='image/jpeg')
     movel = Movel(
         titulo='Prensa de Madeira da Época Colonial',
         preco='30000.001',
+        foto=image,
         descricao='Prensa de farinha de mandioca, madeira maciça. Construída artesanalmente por '
                   'escravos.'
+
     )
 
     movel.save()
@@ -34,7 +45,6 @@ def test_status_code(resp):
 
 @pytest.mark.parametrize(
     'content', [
-        '//s3.amazonaws.com/img.iluria.com/product/466341/A93300/450xN.jpg',
         '30000,00',
         'Prensa de Madeira da Época Colonial',
         'Prensa de farinha de mandioca, madeira maciça. Construída artesanalmente por escravos.',
@@ -42,3 +52,8 @@ def test_status_code(resp):
 )
 def test_index_content(resp, content):
     dj_assert_contains(resp, content)
+
+
+def test_image_url(resp, moveis):
+    movel = moveis[0]
+    dj_assert_contains(resp, movel.foto.url)
