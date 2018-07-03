f6d37867339ff0c7b0bf1074ecccb48a286011dc
renzon <renzon@gmail.com>
2018-07-02 21:26:59 -0300

    Customizado Usuário e Criado admin para Móveis
    close #55

--- antigonovo/moveis/models.py
+++ antigonovo/moveis/models.py
@@ -5,3 +5,9 @@ class Movel(models.Model):
     titulo = models.CharField(max_length=50)
     preco = models.DecimalField(decimal_places=2, max_digits=10)
     descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+
+    def __repr__(self):
+        return f'Movel(titulo={self.titulo!r}, preco={self.preco!r}, descricao={self.descricao!r}'
