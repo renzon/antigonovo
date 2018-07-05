e2cbbfedfcf5e79a480d79b86b18507da95d8198
renzon <renzon@gmail.com>
2018-07-05 09:16:15 -0300

    Implementado
    close #69

--- antigonovo/settings.py
+++ antigonovo/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'core.User'
 
+LOGIN_REDIRECT_URL = LOGOUT_REDIRECT_URL = '/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
