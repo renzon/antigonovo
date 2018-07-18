07e57d4eaac8c53c62a845a098dc73999ef93b8e
renzon <renzon@gmail.com>
2018-07-18 20:51:08 -0300

    Validando dados na unha
    relacionado com #84

--- antigonovo/django_assertions.py
+++ antigonovo/django_assertions.py
@@ -3,3 +3,4 @@ from django.test import TestCase
 _test = TestCase()
 
 dj_assert_contains = _test.assertContains
+dj_assert_in = _test.assertIn
--- antigonovo/moveis/views.py
+++ antigonovo/moveis/views.py
@@ -22,8 +22,23 @@ def new(request):
 
 @login_required
 def create(request):
+    # Extraia os dados do request
     dct = request.POST
+    # Valide os Inputs
+    erros = {}
+    for propriedade in 'titulo descricao'.split():
+        if dct.get(propriedade, '') == '':
+            erros[propriedade] = 'Campo obrigatório.'
     preco = dct['preco'].replace(',', '.')
+    try:
+        float(preco)
+    except Exception:
+        erros['preco'] = 'Digite um valor numérico'
+    # Se tiver erro, retorne para o form preenchendo com dados já enviados e msgs de erro
+    if erros:
+        ctx = {'form': dct, 'erros': erros}
+        return render(request, 'moveis/movel_form.html', context=ctx, status=400)
+    # Se válido, salve no banco e redirecione
     movel = Movel(titulo=dct['titulo'], preco=preco, descricao=dct['descricao'])
     movel.save()
     return redirect(reverse('moveis:index'))
