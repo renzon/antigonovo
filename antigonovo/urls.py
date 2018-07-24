4b8fd26d69f23412c1c8a234da552d69b2e34f65
renzon <renzon@gmail.com>
2018-07-24 18:45:40 -0300

    Consertada listagem de imagens localmente
    close #94

--- antigonovo/urls.py
+++ antigonovo/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -26,6 +27,9 @@ urlpatterns = [
     path('moveis/', include('antigonovo.moveis.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if not settings.AWS_ACCESS_KEY_ID:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
 if settings.DEBUG:
     import debug_toolbar
